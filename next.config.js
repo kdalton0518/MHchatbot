8f3fb567ccf81f406234c3e134f47bbaeb5a7f8c
kdalton0518 <kdalton0518@gmail.com>
2025-01-12 20:42:11 -0500

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: '/api/v1/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://localhost:8000/fastapi/:path*'
+            ? 'https://m-chatbot.vercel.app/fastapi/:path*'
             : '/api/v1/:path*',
       },
     ]
@@ -18,7 +18,7 @@ const nextConfig = {
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: 'https://m-chatbot.vercel.app' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
           {
             key: 'Access-Control-Allow-Headers',
             value:
